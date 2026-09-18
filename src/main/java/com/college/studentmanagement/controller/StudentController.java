1ef2a26225efef9f903faafe6d33752bdadcd383
JaiHarsan <jaiharsan.s2008@gmail.com>
2026-09-18 15:37:22 +0530

    Initial commit - Student Management System

--- src/main/java/com/college/studentmanagement/controller/StudentController.java
+++ src/main/java/com/college/studentmanagement/controller/StudentController.java
@@ -2,17 +2,19 @@ package com.college.studentmanagement.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.sprintion.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.college.studentmanagement.entity.Student;
 import com.college.studentmanagement.service.StudentService;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class StudentController {
 
@@ -22,31 +24,31 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    // GET - test
+    // Test API
     @GetMapping("/hello")
     public String hello() {
         return "Successful";
     }
 
-    // GET - get all students
+    // GET - Get all students
     @GetMapping("/students")
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
     }
 
-    // POST - create student
+    // POST - Create student
     @PostMapping("/students")
     public Student createStudent(@RequestBody Student student) {
         return studentService.createStudent(student);
     }
 
-    // GET - get student by ID
+    // GET - Get student by ID
     @GetMapping("/students/{id}")
     public Student getStudentById(@PathVariable Long id) {
         return studentService.getStudentById(id);
     }
 
-    // PUT - update student
+    // PUT - Update student
     @PutMapping("/students/{id}")
     public Student updateStudent(
             @PathVariable Long id,
@@ -55,9 +57,82 @@ public class StudentController {
         return studentService.updateStudent(id, student);
     }
 
-    // DELETE - delete student
+    // DELETE - Delete student
     @DeleteMapping("/students/{id}")
     public String deleteStudent(@PathVariable Long id) {
         return studentService.deleteStudent(id);
     }
 }
+
+
+
+
+
+
+
+// package com.college.studentmanagement.controller;
+
+// import java.util.List;
+
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
+// import org.sprintion.RequestBody;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import com.college.studentmanagement.entity.Student;
+// import com.college.studentmanagement.service.StudentService;
+
+// @RestController
+// public class StudentController {
+
+//     private final StudentService studentService;
+
+//     public StudentController(StudentService studentService) {
+//         this.studentService = studentService;
+//     }
+
+//     // GET - test
+//     @GetMapping("/hello")
+//     public String hello() {
+//         return "Successful";
+//     }
+
+//     // GET - get all students
+//     @GetMapping("/students")
+//     public List<Student> getAllStudents() {
+//         return studentService.getAllStudents();
+//     }
+
+//     // POST - create student
+//     @PostMapping("/students")
+//     public Student createStudent(@RequestBody Student student) {
+//         return studentService.createStudent(student);
+//     }
+
+//     // GET - get student by ID
+//     @GetMapping("/students/{id}")
+//     public Student getStudentById(@PathVariable Long id) {
+//         return studentService.getStudentById(id);
+//     }
+
+//     // PUT - update student
+//     @PutMapping("/students/{id}")
+//     public Student updateStudent(
+//             @PathVariable Long id,
+//             @RequestBody Student student) {
+
+//         return studentService.updateStudent(id, student);
+//     }
+
+//     // DELETE - delete student
+//     @DeleteMapping("/students/{id}")
+//     public String deleteStudent(@PathVariable Long id) {
+//         return studentService.deleteStudent(id);
+//     }
+// }
+
+
+
