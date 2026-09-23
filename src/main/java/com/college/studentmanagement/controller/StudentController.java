56a837c1af091e3c80e44c53ccfaddd4457ef0fb
Jai Harsan S <jaiharsan.s2008@gmail.com>
2026-09-23 22:46:14 +0530

    Update StudentController.java

--- src/main/java/com/college/studentmanagement/controller/StudentController.java
+++ src/main/java/com/college/studentmanagement/controller/StudentController.java
@@ -1,7 +1,7 @@
 package com.college.studentmanagement.controller;
 
 import java.util.List;
-
+  
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
